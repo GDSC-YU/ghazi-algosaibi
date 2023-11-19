43d6a31ea5699035342f837f9c006043b7ece10d
Khalil Melhem <khalil.i.melhem@gmail.com>
2023-11-20 01:29:36 +0300

    chore: add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,12 @@
+import type { Metadata } from "next";
+
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Ghazi Al Gosaibi",
+  description:
+    "Ghazi Al Gosaibi: a statesman, diplomat, technocrat, academic, poet, and novelist, integral to the fabric of contemporary Saudi society.",
+};
 
 export default function RootLayout({
   children,
