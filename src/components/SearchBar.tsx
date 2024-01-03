02fe481a07c69d0be3a289abe78c7b4ed45dc0c7
Yazeed AlKhalaf <yazeedfady@gmail.com>
2024-01-04 02:34:35 +0300

    add the magnifying glass to the search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import magnifyingGlassSolid from "@/app/images/magnifying-glass-solid.svg";
+import Image from "next/image";
 
 export default function SearchBar({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
@@ -20,15 +22,22 @@ export default function SearchBar({ placeholder }: { placeholder: string }) {
 
   return (
     <div className="mb-12 flex flex-row items-center justify-center px-4">
-      <input
-        type="text"
-        className="bg-secondary text-outline h-12 w-96 rounded-2xl p-2 text-2xl"
-        placeholder={placeholder}
-        onChange={(e) => {
-          handleSearch(e.target.value);
-        }}
-        defaultValue={searchParams.get("query")?.toString()}
-      />
+      <div className="relative">
+        <Image
+          src={magnifyingGlassSolid}
+          alt="أيقونة البحث"
+          className="absolute left-3 top-1/2 -translate-y-1/2 transform"
+        />
+        <input
+          type="text"
+          className="bg-secondary text-outline h-12 w-96 rounded-2xl pl-12 pr-5 text-2xl"
+          placeholder={placeholder}
+          onChange={(e) => {
+            handleSearch(e.target.value);
+          }}
+          defaultValue={searchParams.get("query")?.toString()}
+        />
+      </div>
     </div>
   );
 }
