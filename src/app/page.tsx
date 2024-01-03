8a69fc8357bd8407d1eb8032cf38e38e072cbeb2
Yazeed AlKhalaf <yazeedfady@gmail.com>
2024-01-04 02:25:36 +0300

    make a link from home and back to home from header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
 import { metadata } from "./layout";
 
 export default function Home() {
   const title = String(metadata.title);
 
-  return <h1 className="p-14 text-5xl font-bold">{title}</h1>;
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="p-14 text-5xl font-bold">{title}</h1>
+
+      <Link href="/discover-writings">
+        <p className="bg-primary rounded-2xl px-4 py-2 text-white transition-all hover:scale-105">
+          استكشف المؤلّفات
+        </p>
+      </Link>
+    </div>
+  );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import logo from "@/app/images/logo.png";
 import HeaderButton from "./HeaderButton";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="bg-primary flex h-24 flex-row items-center justify-between gap-4 px-10">
-      <Image src={logo} alt="شعار غازي عبدالرحمن القصيبي" />
+      <Link href={"/"}>
+        <Image src={logo} alt="شعار غازي عبدالرحمن القصيبي" />
+      </Link>
       <div className="flex flex-row items-center justify-center">
         <HeaderButton>ع</HeaderButton>
       </div>
