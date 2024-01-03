4cf96475312c6fbbb9408bdc2ad7bf73cf6b73c6
Yazeed AlKhalaf <yazeedfady@gmail.com>
2024-01-04 01:10:10 +0300

    create the discover writings with its header

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import logo from "@/app/images/logo.png";
+import HeaderButton from "./HeaderButton";
+
+export default function Header() {
+  return (
+    <header className="bg-primary flex h-24 flex-row items-center justify-between px-10">
+      <Image src={logo} alt="شعار غازي عبدالرحمن القصيبي" />
+      <div className="flex flex-row items-center justify-center">
+        <HeaderButton>ع</HeaderButton>
+      </div>
+    </header>
+  );
+}
