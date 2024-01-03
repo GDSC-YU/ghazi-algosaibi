3ead7ac4cab9d1118e8a52eab3a8941e5a180c37
Yazeed AlKhalaf <yazeedfady@gmail.com>
2024-01-04 02:16:54 +0300

    add gap to header flex elements

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import HeaderButton from "./HeaderButton";
 
 export default function Header() {
   return (
-    <header className="bg-primary flex h-24 flex-row items-center justify-between px-10">
+    <header className="bg-primary flex h-24 flex-row items-center justify-between gap-4 px-10">
       <Image src={logo} alt="شعار غازي عبدالرحمن القصيبي" />
       <div className="flex flex-row items-center justify-center">
         <HeaderButton>ع</HeaderButton>
