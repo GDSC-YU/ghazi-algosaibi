fa76fbbc2e446114d27d6240d42c9c329dd5f09b
Khalil Melhem <khalil.i.melhem@gmail.com>
2024-01-05 00:01:57 +0300

    initial hero commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,9 @@
-import Link from "next/link";
-
-import { metadata } from "./layout";
+import Hero from "@components/hero";
 
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col items-center">
-        <h1 className="p-14 text-5xl font-bold">{metadata.title as string}</h1>
-
-        <Link href="/discover-writings">
-          <p className="rounded-2xl bg-primary px-4 py-2 text-white transition-all hover:scale-105">
-            استكشف المؤلّفات
-          </p>
-        </Link>
-      </div>
+      <Hero />
     </>
   );
 }
