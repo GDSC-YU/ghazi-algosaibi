b292f0ff49bc04b5a4a16aaec38f95b1ca90f124
Khalil Melhem <khalil.i.melhem@gmail.com>
2024-01-04 13:45:22 +0300

    header enhancements

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,11 @@
-import Image from "next/image";
-import logo from "@/app/images/logo.png";
 import HeaderButton from "./HeaderButton";
 import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="flex h-24 flex-row items-center justify-between gap-4 bg-primary px-10">
-      <Link href={"/"}>
-        <Image src={logo} alt="شعار غازي عبدالرحمن القصيبي" />
+      <Link href="/" className="text-3xl font-extrabold text-secondary">
+        غَازِي عَبْد الرَّحْمن القصيْبيّ
       </Link>
       <div className="flex flex-row items-center justify-center">
         <HeaderButton>ع</HeaderButton>
