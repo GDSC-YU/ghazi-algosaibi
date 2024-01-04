791d86a7260bb6dfe1b62466f4c6d7ff3edfd53a
Yazeed AlKhalaf <yazeedfady@gmail.com>
2024-01-04 21:39:57 +0300

    enhance the writings ui even more

--- src/components/writings/parts/WritingTile.tsx
+++ src/components/writings/parts/WritingTile.tsx
@@ -1,12 +1,12 @@
 const WritingTile = ({ thing }: { thing: Thing }) => {
   return (
-    <div className="flex flex-col items-center rounded-2xl bg-secondary p-4 text-center">
+    <div className="flex flex-col items-center rounded-2xl bg-secondary p-4 text-center transition-all hover:scale-105 hover:shadow-lg">
       <img
         className="mb-4 rounded-2xl"
         src={thing.imageUrl}
         alt={thing.title}
       />
-      <h3 className="text-xl">{thing.title}</h3>
+      <h3 className="line-clamp-2 text-xl">{thing.title}</h3>
     </div>
   );
 };
