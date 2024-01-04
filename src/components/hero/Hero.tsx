664ba0ebab954a6dd96d1c7c6888620897d37ef7
Khalil Melhem <me@khalil.im>
2024-01-05 00:49:49 +0300

    first commit 👍🏻

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,17 +1,23 @@
+"use client";
+
 import Link from "next/link";
+
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
 const Hero = () => {
   return (
-    <section>
-      <div className="flex flex-col items-center">
-        <h1 className="p-14 text-5xl font-bold">غازي القصيبي</h1>
-        <Link href="/discover-writings">
-          <p className="rounded-2xl bg-primary px-4 py-2 text-white transition-all hover:scale-105">
-            استكشف المؤلّفات
-          </p>
-        </Link>
-      </div>
-    </section>
+    <Parallax pages={1} style={{ top: "0", left: "0" }}>
+      <ParallaxLayer offset={0} speed={0.5}>
+        <div className="flex h-screen w-screen flex-col items-center justify-center bg-primary">
+          <h1 className="p-14 text-5xl font-bold text-white">غازي القصيبي</h1>
+          <Link href="/discover-writings">
+            <p className="rounded-2xl bg-black px-4 py-2 text-white transition-all hover:scale-105">
+              استكشف المؤلّفات
+            </p>
+          </Link>
+        </div>
+      </ParallaxLayer>
+    </Parallax>
   );
 };
 
