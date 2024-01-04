9dfc0f93a5042e399db8ab5172650218cc06ba34
Khalil Melhem <khalil.i.melhem@gmail.com>
2024-01-04 14:06:09 +0300

    hi

--- src/components/writings/parts/WritingTile.tsx
+++ src/components/writings/parts/WritingTile.tsx
@@ -1,8 +1,9 @@
 const WritingTile = ({ thing }: { thing: Thing }) => {
   return (
+    // TODO: rewrite this whole thing
     <div className="h-96 rounded-2xl bg-orange-700 text-center">
       <img
-        className="h-80 w-full rounded-2xl bg-green-700"
+        className="h-80 w-full rounded-t-2xl bg-green-700"
         src={thing.imageUrl}
         alt={thing.title}
       />
