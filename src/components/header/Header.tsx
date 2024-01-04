c0bc3fa63e61951a7eed718f4ab66992396ab841
Yazeed AlKhalaf <yazeedfady@gmail.com>
2024-01-05 00:27:07 +0300

    make it fixed size

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,7 +7,12 @@ const Header = () => {
   return (
     <header className="flex h-24 flex-row items-center justify-between gap-4 bg-primary px-10">
       <Link href={"/"}>
-        <Image src={logo} alt="شعار غازي عبدالرحمن القصيبي" />
+        <Image
+          src={logo}
+          width={292}
+          height={61}
+          alt="شعار غازي عبدالرحمن القصيبي"
+        />
       </Link>
       <div className="flex flex-row items-center justify-center">
         <HeaderButton>ع</HeaderButton>
