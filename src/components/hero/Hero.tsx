458031c304ed9e4f105969b330818b8f1d0714d0
Yazeed AlKhalaf <yazeedfady@gmail.com>
2024-01-05 13:52:41 +0300

    make the parallax images hosted on our server

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,22 +1,18 @@
 "use client";
 
 import { useRef } from "react";
-
 import Link from "next/link";
-
 import { Parallax, ParallaxLayer, IParallax } from "@react-spring/parallax";
 
 import ButtonStyles from "./styles/Button.module.css";
 
-const url = (name: string, wrap = false) =>
-  `${
-    wrap ? "url(" : ""
-  }https://awv3node-homepage.surge.sh/build/assets/${name}.svg${
-    wrap ? ")" : ""
-  }`;
-
 const Hero = () => {
   const parallax = useRef<IParallax>(null!);
+
+  const makeParallaxUrl = (imageName: string, wrap: boolean) => {
+    return `${wrap ? "url(" : ""}/parallax/${imageName}${wrap ? ")" : ""}`;
+  };
+
   return (
     <Parallax ref={parallax} pages={3} className="bg-primary text-white">
       <ParallaxLayer
@@ -34,7 +30,7 @@ const Hero = () => {
         speed={0}
         factor={3}
         style={{
-          backgroundImage: url("stars", true),
+          backgroundImage: makeParallaxUrl("stars.svg", true),
           backgroundSize: "cover",
         }}
       />
@@ -45,66 +41,66 @@ const Hero = () => {
         style={{ pointerEvents: "none" }}
       >
         <img
-          src={url("satellite4")}
+          src={makeParallaxUrl("satellite4.svg", false)}
           style={{ width: "15%", marginLeft: "70%" }}
         />
       </ParallaxLayer>
 
       <ParallaxLayer offset={1} speed={0.8} style={{ opacity: 0.1 }}>
         <img
-          src={url("cloud")}
+          src={makeParallaxUrl("cloud.svg", false)}
           style={{ display: "block", width: "20%", marginLeft: "55%" }}
         />
         <img
-          src={url("cloud")}
+          src={makeParallaxUrl("cloud.svg", false)}
           style={{ display: "block", width: "10%", marginLeft: "15%" }}
         />
       </ParallaxLayer>
 
       <ParallaxLayer offset={1.75} speed={0.5} style={{ opacity: 0.1 }}>
         <img
-          src={url("cloud")}
+          src={makeParallaxUrl("cloud.svg", false)}
           style={{ display: "block", width: "20%", marginLeft: "70%" }}
         />
         <img
-          src={url("cloud")}
+          src={makeParallaxUrl("cloud.svg", false)}
           style={{ display: "block", width: "20%", marginLeft: "40%" }}
         />
       </ParallaxLayer>
 
       <ParallaxLayer offset={1} speed={0.2} style={{ opacity: 0.2 }}>
         <img
-          src={url("cloud")}
+          src={makeParallaxUrl("cloud.svg", false)}
           style={{ display: "block", width: "10%", marginLeft: "10%" }}
         />
         <img
-          src={url("cloud")}
+          src={makeParallaxUrl("cloud.svg", false)}
           style={{ display: "block", width: "20%", marginLeft: "75%" }}
         />
       </ParallaxLayer>
 
       <ParallaxLayer offset={1.6} speed={-0.1} style={{ opacity: 0.4 }}>
         <img
-          src={url("cloud")}
+          src={makeParallaxUrl("cloud.svg", false)}
           style={{ display: "block", width: "20%", marginLeft: "60%" }}
         />
         <img
-          src={url("cloud")}
+          src={makeParallaxUrl("cloud.svg", false)}
           style={{ display: "block", width: "25%", marginLeft: "30%" }}
         />
         <img
-          src={url("cloud")}
+          src={makeParallaxUrl("cloud.svg", false)}
           style={{ display: "block", width: "10%", marginLeft: "80%" }}
         />
       </ParallaxLayer>
 
       <ParallaxLayer offset={2.6} speed={0.4} style={{ opacity: 0.6 }}>
         <img
-          src={url("cloud")}
+          src={makeParallaxUrl("cloud.svg", false)}
           style={{ display: "block", width: "20%", marginLeft: "5%" }}
         />
         <img
-          src={url("cloud")}
+          src={makeParallaxUrl("cloud.svg", false)}
           style={{ display: "block", width: "15%", marginLeft: "75%" }}
         />
       </ParallaxLayer>
@@ -119,7 +115,10 @@ const Hero = () => {
           pointerEvents: "none",
         }}
       >
-        <img src={url("earth")} style={{ width: "60%" }} />
+        <img
+          src={makeParallaxUrl("earth.svg", false)}
+          style={{ width: "60%" }}
+        />
       </ParallaxLayer>
 
       <ParallaxLayer
@@ -128,7 +127,7 @@ const Hero = () => {
         style={{
           backgroundSize: "80%",
           backgroundPosition: "center",
-          backgroundImage: url("clients", true),
+          backgroundImage: makeParallaxUrl("clients.svg", true),
         }}
       />
 
@@ -185,7 +184,10 @@ const Hero = () => {
         }}
         onClick={() => parallax.current.scrollTo(0)}
       >
-        <img src={url("clients-main")} style={{ width: "40%" }} />
+        <img
+          src={makeParallaxUrl("clients-main.svg", false)}
+          style={{ width: "40%" }}
+        />
       </ParallaxLayer>
     </Parallax>
   );
