4395d0298792ece9f92e523f70df743a1529d0ed
Khalil Melhem <khalil.i.melhem@gmail.com>
2024-01-05 13:16:14 +0300

    morning

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -6,7 +6,8 @@ import Link from "next/link";
 
 import { Parallax, ParallaxLayer, IParallax } from "@react-spring/parallax";
 
-// Little helpers ...
+import ButtonStyles from "./styles/Button.module.css";
+
 const url = (name: string, wrap = false) =>
   `${
     wrap ? "url(" : ""
@@ -17,178 +18,176 @@ const url = (name: string, wrap = false) =>
 const Hero = () => {
   const parallax = useRef<IParallax>(null!);
   return (
-    <div style={{ width: "100%", height: "100%" }}>
-      <Parallax ref={parallax} pages={3} className="bg-primary text-white">
-        <ParallaxLayer
-          offset={1}
-          speed={1}
-          style={{ backgroundColor: "#805E73" }}
+    <Parallax ref={parallax} pages={3} className="bg-primary text-white">
+      <ParallaxLayer
+        offset={1}
+        speed={1}
+        style={{ backgroundColor: "#805E73" }}
+      />
+      <ParallaxLayer
+        offset={2}
+        speed={1}
+        style={{ backgroundColor: "#87BCDE" }}
+      />
+      <ParallaxLayer
+        offset={0}
+        speed={0}
+        factor={3}
+        style={{
+          backgroundImage: url("stars", true),
+          backgroundSize: "cover",
+        }}
+      />
+
+      <ParallaxLayer
+        offset={1.3}
+        speed={-0.3}
+        style={{ pointerEvents: "none" }}
+      >
+        <img
+          src={url("satellite4")}
+          style={{ width: "15%", marginLeft: "70%" }}
         />
-        <ParallaxLayer
-          offset={2}
-          speed={1}
-          style={{ backgroundColor: "#87BCDE" }}
+      </ParallaxLayer>
+
+      <ParallaxLayer offset={1} speed={0.8} style={{ opacity: 0.1 }}>
+        <img
+          src={url("cloud")}
+          style={{ display: "block", width: "20%", marginLeft: "55%" }}
         />
-        <ParallaxLayer
-          offset={0}
-          speed={0}
-          factor={3}
-          style={{
-            backgroundImage: url("stars", true),
-            backgroundSize: "cover",
-          }}
+        <img
+          src={url("cloud")}
+          style={{ display: "block", width: "10%", marginLeft: "15%" }}
         />
+      </ParallaxLayer>
 
-        <ParallaxLayer
-          offset={1.3}
-          speed={-0.3}
-          style={{ pointerEvents: "none" }}
-        >
-          <img
-            src={url("satellite4")}
-            style={{ width: "15%", marginLeft: "70%" }}
-          />
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={1} speed={0.8} style={{ opacity: 0.1 }}>
-          <img
-            src={url("cloud")}
-            style={{ display: "block", width: "20%", marginLeft: "55%" }}
-          />
-          <img
-            src={url("cloud")}
-            style={{ display: "block", width: "10%", marginLeft: "15%" }}
-          />
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={1.75} speed={0.5} style={{ opacity: 0.1 }}>
-          <img
-            src={url("cloud")}
-            style={{ display: "block", width: "20%", marginLeft: "70%" }}
-          />
-          <img
-            src={url("cloud")}
-            style={{ display: "block", width: "20%", marginLeft: "40%" }}
-          />
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={1} speed={0.2} style={{ opacity: 0.2 }}>
-          <img
-            src={url("cloud")}
-            style={{ display: "block", width: "10%", marginLeft: "10%" }}
-          />
-          <img
-            src={url("cloud")}
-            style={{ display: "block", width: "20%", marginLeft: "75%" }}
-          />
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={1.6} speed={-0.1} style={{ opacity: 0.4 }}>
-          <img
-            src={url("cloud")}
-            style={{ display: "block", width: "20%", marginLeft: "60%" }}
-          />
-          <img
-            src={url("cloud")}
-            style={{ display: "block", width: "25%", marginLeft: "30%" }}
-          />
-          <img
-            src={url("cloud")}
-            style={{ display: "block", width: "10%", marginLeft: "80%" }}
-          />
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={2.6} speed={0.4} style={{ opacity: 0.6 }}>
-          <img
-            src={url("cloud")}
-            style={{ display: "block", width: "20%", marginLeft: "5%" }}
-          />
-          <img
-            src={url("cloud")}
-            style={{ display: "block", width: "15%", marginLeft: "75%" }}
-          />
-        </ParallaxLayer>
-
-        <ParallaxLayer
-          offset={2.5}
-          speed={-0.4}
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            pointerEvents: "none",
-          }}
-        >
-          <img src={url("earth")} style={{ width: "60%" }} />
-        </ParallaxLayer>
-
-        <ParallaxLayer
-          offset={2}
-          speed={-0.3}
-          style={{
-            backgroundSize: "80%",
-            backgroundPosition: "center",
-            backgroundImage: url("clients", true),
-          }}
+      <ParallaxLayer offset={1.75} speed={0.5} style={{ opacity: 0.1 }}>
+        <img
+          src={url("cloud")}
+          style={{ display: "block", width: "20%", marginLeft: "70%" }}
         />
+        <img
+          src={url("cloud")}
+          style={{ display: "block", width: "20%", marginLeft: "40%" }}
+        />
+      </ParallaxLayer>
 
-        <ParallaxLayer
-          offset={0}
-          speed={0.1}
-          onClick={() => parallax.current.scrollTo(1)}
-          className="flex flex-col items-center justify-center gap-y-7"
-        >
-          <img
-            src="/ghazi/ghazi.png"
-            alt="Ghazi Algosaibi Image"
-            className="w-2/5 md:w-[30%] xl:w-1/5"
-          />
-          <h1 className="text-3xl font-bold md:text-4xl">من هو غازي؟</h1>
-          <div className="flex gap-x-2">
-            <button
-              onClick={() => parallax.current.scrollTo(1)}
-              className="rounded-2xl bg-black px-4 py-2 text-white transition-all hover:scale-105"
-            >
-              اقرأ القصّة
-            </button>
-            <Link
-              href="/discover-writings"
-              className="rounded-2xl bg-black px-4 py-2 text-white transition-all hover:scale-105"
-              onClick={(e) => {
-                e.stopPropagation();
-              }}
-            >
-              استكشف المؤلّفات
-            </Link>
-          </div>
-        </ParallaxLayer>
+      <ParallaxLayer offset={1} speed={0.2} style={{ opacity: 0.2 }}>
+        <img
+          src={url("cloud")}
+          style={{ display: "block", width: "10%", marginLeft: "10%" }}
+        />
+        <img
+          src={url("cloud")}
+          style={{ display: "block", width: "20%", marginLeft: "75%" }}
+        />
+      </ParallaxLayer>
 
-        <ParallaxLayer
-          offset={1}
-          speed={0.1}
-          onClick={() => parallax.current.scrollTo(2)}
-          className="flex flex-col items-center justify-center gap-y-7"
-        >
-          <h1 className="text-center text-3xl font-bold md:text-4xl">
-            غازي بن عبد الرحمن القصيبي، (مارس 1940 - 15 أغسطس 2010) <br />
-            شاعر وأديب وكاتب وسفير دبلوماسي ووزير سعودي.
-          </h1>
-        </ParallaxLayer>
+      <ParallaxLayer offset={1.6} speed={-0.1} style={{ opacity: 0.4 }}>
+        <img
+          src={url("cloud")}
+          style={{ display: "block", width: "20%", marginLeft: "60%" }}
+        />
+        <img
+          src={url("cloud")}
+          style={{ display: "block", width: "25%", marginLeft: "30%" }}
+        />
+        <img
+          src={url("cloud")}
+          style={{ display: "block", width: "10%", marginLeft: "80%" }}
+        />
+      </ParallaxLayer>
 
-        <ParallaxLayer
-          offset={2}
-          speed={-0}
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-          onClick={() => parallax.current.scrollTo(0)}
-        >
-          <img src={url("clients-main")} style={{ width: "40%" }} />
-        </ParallaxLayer>
-      </Parallax>
-    </div>
+      <ParallaxLayer offset={2.6} speed={0.4} style={{ opacity: 0.6 }}>
+        <img
+          src={url("cloud")}
+          style={{ display: "block", width: "20%", marginLeft: "5%" }}
+        />
+        <img
+          src={url("cloud")}
+          style={{ display: "block", width: "15%", marginLeft: "75%" }}
+        />
+      </ParallaxLayer>
+
+      <ParallaxLayer
+        offset={2.5}
+        speed={-0.4}
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          pointerEvents: "none",
+        }}
+      >
+        <img src={url("earth")} style={{ width: "60%" }} />
+      </ParallaxLayer>
+
+      <ParallaxLayer
+        offset={2}
+        speed={-0.3}
+        style={{
+          backgroundSize: "80%",
+          backgroundPosition: "center",
+          backgroundImage: url("clients", true),
+        }}
+      />
+
+      <ParallaxLayer
+        offset={0}
+        speed={0.1}
+        onClick={() => parallax.current.scrollTo(1)}
+        className="flex flex-col items-center justify-center gap-y-7"
+      >
+        <img
+          src="/ghazi/ghazi.png"
+          alt="Ghazi Algosaibi Image"
+          className="w-52 md:w-72"
+        />
+        <h1 className="text-3xl font-bold md:text-4xl">من هو غازي؟</h1>
+        <div className="flex gap-x-2">
+          <button
+            onClick={() => parallax.current.scrollTo(1)}
+            className={ButtonStyles.heroButton}
+          >
+            اقرأ القصّة
+          </button>
+          <Link
+            href="/discover-writings"
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+            className={ButtonStyles.heroButton}
+          >
+            استكشف المؤلّفات
+          </Link>
+        </div>
+      </ParallaxLayer>
+
+      <ParallaxLayer
+        offset={1}
+        speed={0.1}
+        onClick={() => parallax.current.scrollTo(2)}
+        className="flex flex-col items-center justify-center gap-y-7"
+      >
+        <h1 className="text-center text-3xl font-bold md:text-4xl">
+          غازي بن عبد الرحمن بن حسن بن عبدالله القصيبي، <br />
+          شاعر وأديب وكاتب وسفير دبلوماسي ووزير سعودي.
+        </h1>
+      </ParallaxLayer>
+
+      <ParallaxLayer
+        offset={2}
+        speed={-0}
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+        onClick={() => parallax.current.scrollTo(0)}
+      >
+        <img src={url("clients-main")} style={{ width: "40%" }} />
+      </ParallaxLayer>
+    </Parallax>
   );
 };
 
