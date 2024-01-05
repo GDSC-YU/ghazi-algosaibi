e8ced1c8c6564137940c6cca020b4a097758d92c
Yazeed AlKhalaf <yazeedfady@gmail.com>
2024-01-05 23:50:10 +0300

    add padding for small phones

--- src/components/hero/parts/TextBox.tsx
+++ src/components/hero/parts/TextBox.tsx
@@ -1,6 +1,6 @@
 const TextBox = ({ children }: { children: React.ReactNode }) => {
   return (
-    <p className="max-w-xs text-center text-sm font-semibold md:max-w-md md:text-base lg:max-w-lg lg:text-lg">
+    <p className="max-w-xs p-6 text-center text-sm font-semibold md:max-w-md md:text-base lg:max-w-lg lg:text-lg">
       {children}
     </p>
   );
