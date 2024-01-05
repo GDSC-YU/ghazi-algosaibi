85d48459b2ed093a384645234aab5c4fc2b20b4d
Khalil Melhem <me@khalil.im>
2024-01-05 19:03:03 +0300

    refactor & add layer 3

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -4,7 +4,7 @@ import { useRef } from "react";
 
 import { Parallax, ParallaxLayer, IParallax } from "@react-spring/parallax";
 
-import { Layer1, Layer2 } from "./layers";
+import { Layer1, Layer2, Layer3 } from "./layers";
 
 import { makeParallaxUrl } from "@lib/utils";
 
@@ -14,14 +14,14 @@ const Hero = () => {
   return (
     <Parallax
       ref={parallax}
-      pages={2}
-      className="scrollbar-none bg-black text-white"
+      pages={3}
+      className="bg-black text-white scrollbar-none"
     >
       {/* background stars */}
       <ParallaxLayer
         offset={0}
         speed={0}
-        factor={2}
+        factor={3}
         style={{
           backgroundImage: makeParallaxUrl("stars.svg", true),
           backgroundSize: "cover",
@@ -30,6 +30,7 @@ const Hero = () => {
       {/* background stars */}
       <Layer1 parallax={parallax} />
       <Layer2 parallax={parallax} />
+      <Layer3 parallax={parallax} />
     </Parallax>
   );
 };
