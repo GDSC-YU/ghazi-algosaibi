91d0cb16cd96497b2dee561cb225692c24c608b1
Khalil Melhem <khalil.i.melhem@gmail.com>
2024-01-05 14:25:26 +0300

    update plugin configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("tailwind-scrollbar")({ nocompatible: true })],
+  plugins: [require("tailwind-scrollbar")],
 };
 export default config;
