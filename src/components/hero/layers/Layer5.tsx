793e89f821e5fb689d6d78250cfc4b0e5c998927
Yazeed AlKhalaf <yazeedfady@gmail.com>
2024-01-05 22:24:36 +0300

    add our section

---
+++ src/components/hero/layers/Layer5.tsx
@@ -0,0 +1,69 @@
+import { IParallax, ParallaxLayer } from "@react-spring/parallax";
+import { MutableRefObject } from "react";
+import Link from "next/link";
+
+const Layer5 = ({ parallax }: { parallax: MutableRefObject<IParallax> }) => {
+  return (
+    <>
+      <ParallaxLayer
+        offset={4}
+        speed={0.1}
+        className="flex items-center justify-center"
+      >
+        <div className="text-center text-4xl font-bold text-white">
+          <p>
+            صنع من قبل طلاب{" "}
+            <a
+              className="text-orange-400 transition-transform duration-200 ease-in-out hover:scale-105"
+              href="https:/yu.edu.sa"
+              target="_blank"
+            >
+              جامعة اليمامة
+            </a>{" "}
+            🎓:
+          </p>
+
+          <br />
+
+          <div className="flex flex-row items-center justify-center gap-6 text-2xl">
+            <a
+              className="transition-transform duration-200 ease-in-out hover:scale-105"
+              href={`https://khalil.im/`}
+              target="_blank"
+            >
+              <div className="flex flex-col items-center gap-4">
+                <img
+                  className="h-20 w-20 rounded-full"
+                  src="/khalil.jpg"
+                  alt="خليل ملحم"
+                />
+
+                <p>خليل ملحم</p>
+              </div>
+            </a>
+
+            <span className="text-6xl">|</span>
+
+            <a
+              className="transition-transform duration-200 ease-in-out hover:scale-105"
+              href={`https://yazeed.alkhalaf.dev`}
+              target="_blank"
+            >
+              <div className="flex flex-col items-center gap-4">
+                <img
+                  className="h-20 w-20 rounded-full"
+                  src="https://yazeed.alkhalaf.dev/images/me-grey.jpg"
+                  alt="يزيد الخلف"
+                />
+
+                <p>يزيد الخلف</p>
+              </div>
+            </a>
+          </div>
+        </div>
+      </ParallaxLayer>
+    </>
+  );
+};
+
+export default Layer5;
--- src/components/hero/layers/index.ts
+++ src/components/hero/layers/index.ts
@@ -2,3 +2,4 @@ export { default as Layer1 } from "./Layer1";
 export { default as Layer2 } from "./Layer2";
 export { default as Layer3 } from "./Layer3";
 export { default as Layer4 } from "./Layer4";
+export { default as Layer5 } from "./Layer5";
