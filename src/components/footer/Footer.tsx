7d4ad53f664dbaeebe02c9d02a6d5d87d57ff297
Khalil Melhem <me@khalil.im>
2024-01-18 18:45:52 +0300

    quick traffic light fix
    * yes, i got the idea while driving

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,21 +2,23 @@ import { makeParallaxUrl } from "@lib/utils";
 
 const Footer = () => {
   return (
-    <footer
-      className="flex flex-col items-center bg-primary p-6"
-      style={{
-        backgroundImage: makeParallaxUrl("/stars.svg", true),
-        objectFit: "cover",
-      }}
-    >
-      <a
-        href="/?offset=5"
-        className="text-center font-bold text-white transition-transform duration-200 ease-in-out hover:scale-105"
+    <div className="py-10">
+      <footer
+        className="absolute bottom-0 left-0 right-0 flex w-full flex-col items-center bg-primary py-6"
+        style={{
+          backgroundImage: makeParallaxUrl("/stars.svg", true),
+          objectFit: "cover",
+        }}
       >
-        صنع باستخدام الكثير من القهوة <br className="md:hidden" /> من قبل طلّاب
-        جامعة اليمامة ☕️
-      </a>
-    </footer>
+        <a
+          href="/?offset=5"
+          className="text-center font-bold text-white transition-transform duration-200 ease-in-out hover:scale-105"
+        >
+          صنع باستخدام الكثير من القهوة <br className="md:hidden" />
+          من قبل طلّاب جامعة اليمامة ☕️
+        </a>
+      </footer>
+    </div>
   );
 };
 
